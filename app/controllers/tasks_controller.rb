b0855b231e3e25a42ec0fafe08aff910b6decbb3
KeijiTadokoro <gccx.kt.0925@icloud.com>
2020-10-17 10:37:07 +0000

    20201017Task編集画面遷移

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,41 @@
 class TasksController < ApplicationController
 protect_from_forgery
   
-  def index
-    @tasks = Task.all
-    
-  end
-  
-  def show
-    
-  end
-  
-  def edit
-    @task = Task.find(params[:id])
-  end
-  
-  def new
-    @task = Task.new
-  end
-  
-  def create
-    # @task = Task.new(task_params)
-    @task = current_user.tasks.build(task_params)
-    if @task.save
-      flash[:success] = 'タスクを新規作成しました。'
-      redirect_to tasks_url
-    else
-      render :new
+    def index
+      @tasks = Task.all
+      
     end
-  end
-  
-  private
-     def task_params
-      params.require(:task).permit(:name,:created_at)
-     end
+    
+    def show
+      
+    end
+    
+    def edit
+      @task = Task.find(params[:id])
+    end
+    
+    def new
+      @task = Task.new
+    end
+    
+    def update
+      @task = Task.find(params[:id])
+      redirect_to tasks_url
+    end
+    
+    def create
+      # @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
+      if @task.save
+        flash[:success] = 'タスクを新規作成しました。'
+        redirect_to tasks_url
+      else
+        render :new
+      end
+    end    
+      
+    private
+       def task_params
+        params.require(:task).permit(:name,:created_at,:description)
+       end
 end
