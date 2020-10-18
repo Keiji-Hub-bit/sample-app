d195a51941aa2833340b6d6ad9ac8d90245bdef1
KeijiTadokoro <gccx.kt.0925@icloud.com>
2020-10-18 15:43:00 +0000

    20201019　UPDATE完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,46 @@
 class TasksController < ApplicationController
 protect_from_forgery
   
-    def index
-      @tasks = Task.all
-      
-    end
-    
-    def show
-      
-    end
-    
-    def edit
-      @task = Task.find(params[:id])
-    end
-    
-    def new
-      @task = Task.new
-    end
-    
-    def update
-      @task = Task.find(params[:id])
-      @task = current_user.tasks.build(task_params)
-      redirect_to task_url
-    end
-    
-    def create
-      # @task = Task.new(task_params)
-      @task = current_user.tasks.build(task_params)
-      if @task.save
-        flash[:success] = 'タスクを新規作成しました。'
-        redirect_to tasks_url
-      else
-        render :new
+      def index
+        @tasks = Task.all
+        
       end
-    end    
       
-    private
+      def show
+        @task = Task.find(params[:id])
+      end
+      
+      def edit
+        @task = Task.find(params[:id])
+      end
+      
+      def new
+        @task = Task.new
+      end
+      
+      def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+         if @task.save
+        flash[:success] = 'タスクを更新しました。'
+        redirect_to task_url
+         else
+         render:edit
+         end
+      end 
+      
+      def create
+        # @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
+        if @task.save
+          flash[:success] = 'タスクを新規作成しました。'
+          redirect_to tasks_url
+        else
+          render :new
+        end
+      end    
+      
+  private
        def task_params
         params.require(:task).permit(:name,:created_at,:description)
        end
